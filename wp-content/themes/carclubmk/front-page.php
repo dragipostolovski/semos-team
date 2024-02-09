3d6860750b0cb1b002e68342aebbd6916fb2bd71
dimesemos <dime.v.semosedukacija@gmail.com>
2024-02-09 17:41:22 +0100

    new theme

---
+++ wp-content/themes/carclubmk/front-page.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mystyle.css">
+    <?php wp_head(); ?>
+</head>
+<body>
+    
+    <?php if( have_posts() ):
+        while ( have_posts() ): 
+            the_post(); ?>
+
+                <header>
+                    <nav></nav>
+                </header>
+
+                <main class="main">
+                    <article>
+                        <div class="container">
+                            <?php the_content();?>
+                        </div>
+                    </article>
+                </main>
+
+                <footer>
+
+                </footer>
+
+            <?php
+        endwhile;
+    endif; ?>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
