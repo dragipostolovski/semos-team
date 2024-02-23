b32d20c344d0dba6595de1eb0b5cf7e6d9f0c47b
Dragi Postolovski <dpostolovskimk@gmail.com>
2024-02-23 22:53:31 +0100

    footer

--- wp-content/themes/carclubmk/footer.php
+++ wp-content/themes/carclubmk/footer.php
@@ -2,8 +2,10 @@
 
 
 
-    <footer>
-
+    <footer class="site-footer">
+        <div class="site-footer__inner container">
+            <p class="site-footer__legacy">Our footer content will go here.</p>
+        </div>
     </footer>
 </div><!-- end .site-content -->
 <?php wp_footer(); ?>
