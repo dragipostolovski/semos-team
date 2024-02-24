2e11017ee7759053ddcbea7da684f4fb784382d4
Dragi Postolovski <dpostolovskimk@gmail.com>
2024-02-24 20:18:09 +0100

    template files

---
+++ wp-content/themes/carclubmk/exercises/template-files.php
@@ -0,0 +1,12 @@
+<?php
+
+// @link: https://levelup.video/tutorials/modern-wordpress-102/templating
+
+// index.php
+// functions.php
+// style.css
+
+// page.php
+// single.php
+// front-page.php
+
