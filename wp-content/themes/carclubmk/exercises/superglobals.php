2aeddba46790198e5ac722154477e48bd354020e
Dragi Postolovski <dpostolovskimk@gmail.com>
2024-02-25 23:31:22 +0100

    superglobals

---
+++ wp-content/themes/carclubmk/exercises/superglobals.php
@@ -0,0 +1,27 @@
+<?php
+
+// @link: https://www.php.net/manual/en/language.variables.superglobals.php
+
+// $GLOBALS
+// $_SERVER - Server and execution environment information
+// $_GET
+// $_POST
+// $_FILES
+// $_COOKIE
+// $_SESSION
+// $_REQUEST
+// $_ENV
+
+
+// 1. $_SERVER - Server and execution environment information
+
+// SERVER_NAME
+// - The name of the server host under which the current script is executing. If the script is running on a virtual host, this will be the value defined for that virtual host.
+// REQUEST_TIME
+// - The timestamp of the start of the request.
+// REMOTE_ADDR
+// - The IP address from which the user is viewing the current page.
+// REMOTE_HOST
+// - The Host name from which the user is viewing the current page. The reverse dns lookup is based on the REMOTE_ADDR of the user.
+//REQUEST_URI
+// -The URI which was given in order to access this page; for instance, '/index.html'.
